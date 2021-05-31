910faea62e427b5364c6b8acfa6a7ae6598bb929
Manu Martinez <mantinez@gmail.com>
2021-05-31 00:17:45 -0300

    agrego queries para el punto 2

---
+++ entrega TP3/queriesTP3.js
@@ -0,0 +1,125 @@
+// SE INDICAN LAS QUERIES USADAS EN EL TP SEPARADAS POR CONSIGNA:
+
+// 5)
+
+// Crear DB
+db.dropDatabase() // en caso de confundirse
+
+use ecommerce
+
+// Crear coleccion
+db.createCollection("products")
+
+// ver colecciones
+db.getCollectionNames()
+
+// insertar documento
+db.products.insertOne({
+	name: "Caldera Caldaia Duo",
+	price: 140000,
+})
+
+// recuperar (todos) los documentos
+db.products.find().pretty()
+
+// 6)
+
+// insertar varios documentos
+db.products.insertMany([
+	{name:"Caldera Orbis 230", price:77000, tags: ["gas", "digital"]},
+	{name:"Caldera Ariston Clas", price:127000, tags: ["gas envasado", "termostato"]},
+	{name:"Caldera Caldaia S30", price:133000},
+	{name:"Caldera Mural Orbis 225cto", price:100000, tags: ["gas", "digital", "termostato"]} 
+])
+
+// recuperar productos de $100.000 o MENOS
+db.products.find({
+	price: {
+		$lte: 100000,
+	}
+})
+
+// productos con etiqueta "digital"
+db.products.find({
+	tags: "digital"
+})
+
+// no tienen etiquetas (atributo ausente)
+db.products.find({
+	tags: {
+		$exists: false,
+	}
+})
+
+// incluyen "Orbis" en el nombre
+db.products.find({
+	"name": {
+		$regex: /Orbis/,
+	}
+})
+
+// incluyen "Orbis" en el nombre y cuestan menos de $100.000
+db.products.find({
+	name: {
+		$regex: /Orbis/,
+	},
+	price: {
+		$lt: 100000,
+	}
+})
+
+// misma que anterior pero solo proyecta el nombre
+db.products.find({
+	name: {
+		$regex: /Orbis/,
+	},
+	price: {
+		$lt: 100000,
+	}
+},
+{
+	name: true,
+	_id: false,
+})
+
+// 7)
+
+// actualizar precio de "Caldera Caldaia S30" a $150.000
+db.products.updateOne({
+	name: "Caldera Caldaia S30"
+},
+{
+	$set: {
+		price: 150000,
+	}
+})
+
+// crear array de tags para el mismo producto
+db.products.updateOne({
+	name: "Caldera Caldaia S30"
+},
+{
+	$set: {
+		tags: [],
+	}
+})
+
+// agregar el tag "digital" al mismo producto
+db.products.updateOne({
+	name: "Caldera Caldaia S30"
+},
+{
+	$push: {
+		tags: "digital",
+	}
+})
+
+// incrementa en 10% el precio de todas las calderas con tag "digital"
+db.products.updateMany({
+	tags: "digital",
+},
+{
+	$mul: {
+		price: 1.1,
+	}
+})
